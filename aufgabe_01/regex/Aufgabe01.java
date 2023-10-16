f81aa04fc20b065fe71a8d322ca60d080942d700
tobias-mack <80970952+tobias-mack@users.noreply.github.com>
2023-10-16 18:29:49 +0200

    Update Aufgabe01.java

--- aufgabe_01/regex/Aufgabe01.java
+++ aufgabe_01/regex/Aufgabe01.java
@@ -1,40 +1,46 @@
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
-public class Aufgabe01 {
+public class SPKOone {
     public static void main(String[] args) {
-
-        String[] input = {
-                "xxx %d yyy%n",
-                "xxx%1$d yyy",
-                "%1$-02.3dyyy"
+        String[] inputs = {
+            "xxx %d yyy%n",
+            "xxx% 012d yyy%%",
+            "xxx%1$d yyy",
+            "%1$0+(32.10fyyy",
+            "Wochentag: %tA Uhrzeit: %tT"
         };
 
+        for (String input : inputs) {
+            findFormatSpecifiers(input);
+        }
+    }
 
-        String txt = "";
-        String frmt = "";
-        Pattern p = Pattern.compile("%([1-9][0-9]*\\$)?(-?[0-9]*)?\\.?([0-9]*)?[dn]");
-        //Pattern p = Pattern.compile("\\$");
-        for (String str : input) {
-            int i = 0;
-            Matcher m = p.matcher(str);
-            while (m.find() == true) {
-                if (i != m.start()) {
-                    txt = str.substring(i, m.start());
-                    System.out.print("TEXT(\"" + txt + "\")");
-                }
-                frmt = str.substring(m.start(), m.end());
-                System.out.print("FORMAT(\"" + frmt + "\")");
-                i = m.end();
+    public static void findFormatSpecifiers(String input) {
+        Pattern pattern = Pattern.compile("%+.{0,1}[\\w\\d$_-§\"/()=+-]*[.\\d]*[dfegs]*");
+        Matcher matcher = pattern.matcher(input);
+
+        int prevEnd = 0;
+
+        while (matcher.find()) {
+            int start = matcher.start();
+            int end = matcher.end();
+
+            String text = input.substring(prevEnd, start);
+            String specifier = input.substring(start, end);
+
+            if(!text.isEmpty()) {
+                System.out.printf("TEXT(\"%s\")", text);
             }
-            if (i < str.length()) {
-                txt = str.substring(i);
-                System.out.print("TEXT(\"" + txt + "\")");
-            }
-            System.out.println("\n");
+            System.out.printf("FORMAT(\"%s\")", specifier);
+
+            prevEnd = end;
         }
 
+        if (prevEnd < input.length()) {
+            String text = input.substring(prevEnd);
+            System.out.printf("TEXT(\"%s\")", text);
+        }
 
-
+        System.out.println();
     }
 }
